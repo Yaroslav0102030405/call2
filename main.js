b2d77dfee5364952d4bc4c6ead4ce22bec3ada72
Yaroslav Lukovetskyi <lukovetskyi@gmail.com>
2024-02-24 15:17:12 +0200

    fix

--- main.js
+++ main.js
@@ -1,34 +1,61 @@
 const display = document.querySelector(".display");
+// const buttons = document.querySelectorAll(".button");
 const buttons = Array.from(document.querySelectorAll(".button"));
+
+// const canculator = document.querySelector(".calculator");
+// const ac = document.querySelector(".ac");
+// const plusEquals = document.querySelector(".plus-equals");
+// const percent = document.querySelector(".percent");
+// const multiply = document.querySelector(".multiply");
+// const plus = document.querySelector("plus");
+
+// canculator.addEventListener("click", onClick);
+
+// function onClick(e) {
+//   if (e.target === ac) {
+//     display.innerText = "0";
+//   } else if (e.target === plusEquals) {
+//     display.innerText = "-";
+//   } else if (e.target === percent) {
+//     let passedText = display.innerText + "/100";
+//     display.innerText = eval(passedText);
+//   } else if (e.target === multiply) {
+//     display.innerText * e.target.innerText;
+//   } else if (e.target === plus) {
+//     display.innerText += e.target.innerText;
+//   } else if (display.innerText === "0" && e.target.innerText !== ".") {
+//     display.innerText = e.target.innerText;
+//   }
+// }
 
 buttons.map((button) => {
   button.addEventListener("click", (e) => {
-    console.log(e.target.innerText);
+    console.log(e.target.textContent);
     // console.log(e.target);
 
-    switch (e.target.innerText) {
+    switch (e.target.textContent) {
       case "AC":
-        display.innerText = "0";
+        display.textContent = "0";
         break;
       case "=":
         try {
-          display.innerText = eval(display.innerText);
+          display.textContent = eval(display.textContent);
         } catch (e) {
-          display.innerText = "Eror!";
+          display.textContent = "Eror!";
         }
         break;
       case "+/-":
-        display.innerText = "-";
+        display.textContent = "-";
         break;
       case "%":
-        let passedText = display.innerText + "/100";
-        display.innerText = eval(passedText);
+        let passedText = display.textContent + "/100";
+        display.textContent = eval(passedText);
         break;
       default:
-        if (display.innerText === "0" && e.target.innerText !== ".") {
-          display.innerText = e.target.innerText;
+        if (display.textContent === "0" && e.target.textContent !== ".") {
+          display.textContent = e.target.textContent;
         } else {
-          display.innerText += e.target.innerText;
+          display.textContent += e.target.textContent;
         }
     }
   });
